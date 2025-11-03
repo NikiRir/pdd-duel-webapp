b6bb1f0d6e197bca3ad32f818d888f84d5b4de91
Admin <admin@example.com>
2025-11-03 23:55:17 +0300

    up-to-date

--- script.js
+++ script.js
@@ -253,7 +253,7 @@ async function boot(){
        <h2 class="subpage-title">${esc((title || "ПДД ДУЭЛИ").trim())}</h2>
      </header>`;
      
-     host.innerHTML = `<div class="view">${header}${html || ""}</div>`;
+     host.innerHTML = `${header}<div class="view-content">${html || ""}</div>`;
      host.className = "screen";
      host.scrollTop = 0;
    } else {
